fea6adee9ef9cd074db2b72baed67dbfa1a651df
anyer23 <monterrubio_19@hotmail.com>
2023-05-02 21:09:46 -0600

    terminar todas los metodos para peliculas

---
+++ Eje4Pelicula/src/main/java/Atributos/Pelicula.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Atributos;
+
+import java.security.CodeSigner;
+import java.util.Comparator;
+
+/**
+ *
+ * @author monte
+ */
+public class Pelicula {
+
+    private String titulo;
+    private String director;
+    private int duracion;
+
+    public Pelicula() {
+    }
+
+    public Pelicula(String titulo, String director, int duracion) {
+        this.titulo = titulo;
+        this.director = director;
+        this.duracion = duracion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    @Override
+    public String toString() {
+        return "Peliculas " + " Titulo: '" + titulo + "' Director:  '" + director + "' Duracion: '" + duracion + "'";
+    }
+
+    public static Comparator<Pelicula> ordearAse = new Comparator<Pelicula>() {
+
+        @Override
+        public int compare(Pelicula p, Pelicula p1) {
+            return Integer.compare(p.getDuracion(), p1.getDuracion());
+        }
+    };
+
+    public static Comparator<Pelicula> ordearDes = (Pelicula p, Pelicula p1) -> Integer.compare(p1.getDuracion(), p.getDuracion());
+
+    public static Comparator<Pelicula> tituloAlfabeticamente = (Pelicula o, Pelicula o1) -> o.getTitulo().compareTo(o1.getTitulo());
+
+    public static Comparator<Pelicula> directorOrdenadp= new Comparator<Pelicula>() {
+        @Override
+        public int compare(Pelicula o1, Pelicula o2) {
+            return o1.getDirector().compareTo(o2.getDirector());
+                    
+                    }
+    };
+    
+}
---
+++ Eje4Pelicula/src/main/java/Servicio/SerPelicula.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Servicio;
+
+import Atributos.Pelicula;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author monte
+ */
+public class SerPelicula {
+
+    Pelicula peli = new Pelicula();
+    Scanner leer = new Scanner(System.in);
+
+    ArrayList<Pelicula> movie = new ArrayList();
+
+    public void ingresarPeliculas() {
+
+        boolean op = false;
+
+        while (!op) {
+
+            System.out.println("Ingrese el nombre de la pelicula");
+           leer.nextLine();
+            peli.setTitulo(leer.nextLine());
+            System.out.println("Ingrese el director de la pelicula");
+            peli.setDirector(leer.nextLine());
+            System.out.println("Ingrese la duracion de la pelicula");
+            peli.setDuracion(leer.nextInt());
+
+            Pelicula moiv = new Pelicula(peli.getTitulo(), peli.getDirector(), peli.getDuracion());
+            movie.add(moiv);
+
+            System.out.println("Desea agregar otra película Si/No");
+            String otro = leer.next();
+            if (otro.equalsIgnoreCase("n")) {
+                op = true;
+            }
+
+        }
+        System.out.println("Las peliculas ingresadas son ");
+        System.out.println("--------------------------------------------------");
+        mostarPeli();
+    }
+
+    //mostrar todas las peliculas
+    public void mostarPeli() {
+
+        for (Pelicula pel : movie) {
+            System.out.println(pel.toString());
+        }
+    }
+
+    public void peliculasMenor1() {
+        System.out.println("--------------------------------------------------");
+        System.out.println("Peliculas con duracion menor a 60 minutos");
+        System.out.println("--------------------------------------------------");
+        for (Pelicula peliMenor6 : movie) {
+            if (peliMenor6.getDuracion() < 60) {
+                System.out.println(peliMenor6.toString());
+            }
+        }
+    }
+
+    public void ordenMenorMayor() {
+        System.out.println("------------------------------------------------");
+        System.out.println("Peliculas ordenas de (de mayor a menor )");
+        movie.sort(peli.ordearDes);
+        mostarPeli();
+    }
+
+    public void ordenMayorMenor() {
+        System.out.println("------------------------------------------------");
+        System.out.println("Peliculas ordenas de (de menor a mayor)");
+        movie.sort(peli.ordearAse);
+        mostarPeli();
+    }
+
+    public void ordenarTitulo() {
+        System.out.println("---------------------------------------.----------");
+        System.out.println("Titulo de peliculas ordenas de forma alfabetica");
+        movie.sort(peli.tituloAlfabeticamente);
+        mostarPeli();
+    }
+    
+    public void ordenaDirector(){
+       System.out.println("------------------------------------------------");
+        System.out.println("Director  ordenas de forma alfabetica");
+        movie.sort(peli.directorOrdenadp);
+        mostarPeli(); 
+    }
+}
